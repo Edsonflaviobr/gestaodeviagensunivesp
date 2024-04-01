d7d17866b8e437ea494e16424974d80846c1a4f1
Mateus Ferreira <mateusaparecidoferreira@outlook.com>
2024-04-01 10:32:17 -0300

    Versão 1.0.3: Corrigido a página Menu

--- src/Paginas/Menu/Menu.jsx
+++ src/Paginas/Menu/Menu.jsx
@@ -8,53 +8,48 @@ import { Header } from '../../Componentes/Header/Header.jsx';
 import { Title } from '../../Componentes/Title/Title.jsx';
 import { Text } from '../../Componentes/Text/Text.jsx';
 
-const Menu = ({ isAdmin }) => { // Recebe uma propriedade isAdmin para indicar se o usuário é ADM ou não
+const Menu = ({ isAdmin }) => { // Recebe uma propriedade isAdmin para indicar se o usuÃ¡rio Ã© ADM ou nÃ£o
 
     return (
-        <>
+        <div>
             <Header />
-            <body>
-                <div>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <Link to="/cadastro-viagem">
-                                        <img src= {iconViagem} width="150" height="150" />
-                                        <br />
-                                        <strong className="menu-link">Cadastrar Viagem</strong>
-                                    </Link>
-                                </td>
-                                <td>
-                                    <Link to="/cadastro-motorista" >
-                                        <img src={iconMotorista} alt="cadastrarmotorista" width="150" height="150" />
-                                        <br/>
-                                        <strong className="menu-link">Cadastrar Motorista</strong>
-                                    </Link>
-                                </td>
-                                <td>
-                                    {isAdmin ? (
-                                        <Link to="/gerir-usuarios">
-                                            <img src= {iconUsuario} alt="gerirusuario" width="150" height="150" />
-                                            <br />
-                                            <strong className="menu-link">Gerir Usuários</strong>
-                                        </Link>
-                                    ) : (
-                                        <div className="disabled-link">
-                                            <img src= {iconUsuario} alt="gerirusuario" width="150" height="150" />
-                                            <br />
-                                            <strong>Gerir Usuários</strong>
-                                            <span className="disabled-text">Apenas administradores podem acessar</span>
-                                        </div>
-                                    )}
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+            <div class="container">
+                <div class="menu-item">
+                    <a href="/cadastro-viagem">
+                        <img src={iconViagem} alt="viagem" width="150" height="150" />
+                        <br />
+                        <strong class="menu-link">Cadastrar Viagem</strong>
+                    </a>
                 </div>
-            </body>
-        </>
-    )
+                <div class="menu-item">
+                    <a href="/cadastro-motorista">
+                        <img src={iconMotorista} alt="motorista" width="150" height="150" />
+                        <br />
+                        <strong class="menu-link">Cadastrar Motorista</strong>
+                    </a>
+                </div>
+                <div></div>
+                <div class="menu-item">
+                    {isAdmin ? (
+                        <a href="/gerir-usuarios">
+                            <img src={iconUsuario} alt="usuario" width="150" height="150" />
+                            <br />
+                            <strong class="menu-link">Gerir Usuários</strong>
+                        </a>
+                    ) : (
+                        <div class="disabled-link">
+                            <img src={iconUsuario} alt="usuario" width="150" height="150" />
+                            <br/>
+                            <strong>Gerir Usuários</strong>
+                            <br/>
+                            <span class="disabled-text">Apenas administradores podem acessar</span>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+    
 }
 
 export { Menu }
