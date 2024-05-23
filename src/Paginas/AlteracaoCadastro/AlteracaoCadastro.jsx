675df0fa005dcf27e426c9bc40983c91aef2c737
Edsonflaviobr <edsonmflavio@yahoo.com.br>
2024-05-23 14:36:58 -0300

    Versão 1.11

--- src/Paginas/AlteracaoCadastro/AlteracaoCadastro.jsx
+++ src/Paginas/AlteracaoCadastro/AlteracaoCadastro.jsx
@@ -3,7 +3,7 @@ import {React, useEffect} from 'react';
 import { Header } from '../../Componentes/Header/Header.jsx';
 import { Title } from '../../Componentes/Title/Title.jsx';
 import { Text } from '../../Componentes/Text/Text.jsx';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { Input } from '../../Componentes/Input/Input.jsx';
 import { Button } from '../../Componentes/Button/Button.jsx';
 import { MdAccountCircle, MdEmail, MdLock, MdMap } from 'react-icons/md'
@@ -90,8 +90,38 @@ const AlteracaoCadastro = () => {
                         <Input type="matricula" placeholder="Digite sua matrícula" leftIcon={<MdMap />} id="matricula" name="matricula" control={control} rules={{ required: 'Matrícula é obrigatória' }} />
                         {errors.matricula && <span>{errors.matricula.message}</span>}
 
-                        <Input type="number" placeholder="Digite apenas 1 por padrão" leftIcon={<MdMap />} id="roles" name="roles" control={control} rules={{ required: 'Digite apenas 1' }} />
-                        {errors.roles && <span>{errors.roles.message}</span>}
+                        <div className="form-group">
+                          <div className="radio-group">
+                            <Controller
+                              name="roles"
+                              control={control}
+                              rules={{ required: 'Escolha uma opção' }}
+                              render={({ field }) => (
+                                <>
+                                  <label>
+                                    <input
+                                      type="radio"
+                                      value="1"
+                                      checked={field.value === "1"}
+                                      onChange={field.onChange}
+                                    />
+                                    1
+                                  </label>
+                                  <label>
+                                    <input
+                                      type="radio"
+                                      value="2"
+                                      checked={field.value === "2"}
+                                      onChange={field.onChange}
+                                    />
+                                    2
+                                  </label>
+                                </>
+                              )}
+                            />
+                          </div>
+                          {errors.roles && <span>{errors.roles.message}</span>}
+                        </div>
 
                         <Button title="Cadastrar" variant="secondary" type="submit"/>
                     </form>
